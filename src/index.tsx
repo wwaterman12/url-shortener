28df31de161afae58b9115a2bf33dde4b3ab6dc2
Wes Waterman <wwaterman12@gmail.com>
2021-10-24 09:21:38 -0400

    :truck: rename redux store for more intuitive naming convention

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import View from "./view";
-import { store } from "./app/store";
+import { store } from "./store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
--- src/view/View.test.tsx
+++ src/view/View.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "../app/store";
+import { store } from "../store";
 import View from "./index";
 
 test("renders title correctly", () => {
