7fc6186fa10b99c9a10127ca5d91d320acad4aa8
Wes Waterman <wwaterman12@gmail.com>
2021-10-24 10:21:03 -0400

    :truck: rename values from default ones provided by create-react-app

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,10 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import {
+  TypedUseSelectorHook,
+  useDispatch as useLibDispatch,
+  useSelector as useLibSelector,
+} from "react-redux";
 import type { RootState, AppDispatch } from "./index";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useDispatch = () => useLibDispatch<AppDispatch>();
+export const useSelector: TypedUseSelectorHook<RootState> = useLibSelector;
